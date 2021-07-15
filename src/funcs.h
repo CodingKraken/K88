99ef6f83a6b453d0e8d816a5198a1e89c369043f
CodingKraken <52387931+CodingKraken@users.noreply.github.com>
2021-07-14 19:58:50 -0600

    Update funcs.h

--- src/funcs.h
+++ src/funcs.h
@@ -7,10 +7,10 @@ char* InsAsString(struct CPU* cpu);
 
 void ReadByte(struct CPU* cpu, uint8_t memory[0x10000], int location);
 void ReadAddress(struct CPU* cpu, int islow);
+void ReadRegister(struct CPU* cpu, uint8_t Register);
+
 void LoadByte(struct CPU* cpu, uint8_t memory[0x10000], uint16_t dest);
 void LoadAddress(struct CPU* cpu, int islow);
-
-void ReadRegister(struct CPU* cpu, uint8_t Register);
 void LoadRegister(struct CPU* cpu, uint8_t Register);
 
 #endif
